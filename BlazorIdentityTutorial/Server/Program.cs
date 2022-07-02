cbfe3c6ec02b9431a6efc0b8470f4b81801e1b94
Patrick <patrick@godknows.de>
2022-07-02 21:06:46 +0200

    Scaffolding Register Page.

--- BlazorIdentityTutorial/Server/Program.cs
+++ BlazorIdentityTutorial/Server/Program.cs
@@ -3,6 +3,7 @@ using BlazorIdentityTutorial.Server.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
